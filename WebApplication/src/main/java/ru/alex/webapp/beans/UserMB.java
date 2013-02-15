712a355056db9c8de45e474ba88620b9ba01c246
sanea <alexandr.isaenco@gmail.com>
2013-02-15 20:26:01 +0400

    switch to Primefaces

--- WebApplication/src/main/java/ru/alex/webapp/beans/UserMB.java
+++ WebApplication/src/main/java/ru/alex/webapp/beans/UserMB.java
@@ -7,19 +7,17 @@ import ru.alex.webapp.model.Users;
 import ru.alex.webapp.service.UserService;
 
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.RequestScoped;
 import java.util.List;
 
 /**
  * @author Alexander.Isaenco
  */
-@ManagedBean
-@RequestScoped
+@Component
+@Scope(value = "request")
 public class UserMB {
 
-    @ManagedProperty(value = "#{userServiceImpl}")
+    @Autowired
     UserService userService;
 
     List<Users> userList;
