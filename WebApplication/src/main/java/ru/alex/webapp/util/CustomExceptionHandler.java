500f8cde0fa5476e7317bf36e53c0b4d963d42c0
sanea <alexandr.isaenco@gmail.com>
2013-03-07 20:27:32 +0400

    faces ViewExpiredException handle

--- WebApplication/src/main/java/ru/alex/webapp/util/CustomExceptionHandler.java
+++ WebApplication/src/main/java/ru/alex/webapp/util/CustomExceptionHandler.java
@@ -1,5 +1,8 @@
 package ru.alex.webapp.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.faces.FacesException;
 import javax.faces.application.NavigationHandler;
 import javax.faces.application.ViewExpiredException;
@@ -8,12 +11,15 @@ import javax.faces.context.ExceptionHandlerWrapper;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ExceptionQueuedEvent;
 import javax.faces.event.ExceptionQueuedEventContext;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Iterator;
 
 /**
  * @author Alexander.Isaenco
  */
 public class CustomExceptionHandler extends ExceptionHandlerWrapper {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
 
     private ExceptionHandler wrapped;
 
@@ -34,10 +40,16 @@ public class CustomExceptionHandler extends ExceptionHandlerWrapper {
             ExceptionQueuedEventContext context = event.getContext();
             Throwable t = context.getException();
             if (t instanceof ViewExpiredException) {
+                logger.debug("handle ViewExpiredException");
                 FacesContext fc = context.getContext();
+
+                HttpServletRequest req = (HttpServletRequest)fc.getExternalContext().getRequest();
+                String path = req.getServletPath().replace(".xhtml", "").replace(".jsf", "");
+                logger.info("redirect to " + path);
+
                 NavigationHandler navHandler = fc.getApplication().getNavigationHandler();
                 try {
-                    navHandler.handleNavigation(fc, null, "/pages/error?faces-redirect=true&expired=true");
+                    navHandler.handleNavigation(fc, null, path + "?faces-redirect=true&expired=true");
                 } finally {
                     it.remove();
                 }
